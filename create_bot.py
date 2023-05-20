d460ad6e56b8c1a612ee719ad42d50c923b04153
Elisseeff <pavel.eliseev@gmail.com>
2023-05-20 08:28:23 +0300

    777

---
+++ create_bot.py
@@ -0,0 +1,12 @@
+from aiogram import Bot
+from aiogram.dispatcher import Dispatcher
+import json
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+
+file = open('/home/pavel/cfg/config.json', 'r')
+config = json.load(file)
+
+storage = MemoryStorage()
+
+bot = Bot(token = config['rtlab_voice_bot_token'])
+dp = Dispatcher(bot, storage=storage)
